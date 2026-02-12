ae436994f930cda9844ec6c3434ab2cbcc876263
Nishant Dev <nishantdev03@gmail.com>
2026-02-12 10:49:51 +0000

    Refactor code for consistency and readability across services

--- auth-service/index.js
+++ auth-service/index.js
@@ -1,62 +1,59 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 7020;
-const mongoose = require("mongoose")
-const User = require("./models/user")
-const jwt = require("jsonwebtoken")
+const mongoose = require("mongoose");
+const User = require("./models/user");
+const jwt = require("jsonwebtoken");
 
-mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/auth-service", {
+mongoose
+  .connect(process.env.MONGO_URI || "mongodb://localhost/auth-service", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
-    console.log("AUTH SERVICE DB CONNECTED")
-})
+  })
+  .then(() => {
+    console.log("AUTH SERVICE DB CONNECTED");
+  });
 
 app.use(express.json());
 
+app.post("/auth/login", async (req, res) => {
+  const { email, password } = req.body;
 
-app.post("/auth/login", async (req, res) =>{
-    const {email, password} = req.body;
-
-    const user = await User.findOne({email});
-    if(!user){
-        return res.json({message: "User doesn't exits"})
-    } else {
-
-        if(password !== user.password) {
-            return res.json({message: "Passwod icorrect"})
-        }
-
-        const payload = {
-            email,
-            name: user.name,
-        }
-        const token = jwt.sign(payload, "this_is_secret");
-        return res.json({token: token})
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.json({ message: "User doesn't exits" });
+  } else {
+    if (password !== user.password) {
+      return res.json({ message: "Passwod icorrect" });
     }
-})
 
+    const payload = {
+      email,
+      name: user.name,
+    };
+    const token = jwt.sign(payload, "this_is_secret");
+    return res.json({ token: token });
+  }
+});
 
 app.post("/auth/register", async (req, res) => {
-    const {email, password, name} = req.body;
+  const { email, password, name } = req.body;
 
-    const userExists = await User.findOne({email});
+  const userExists = await User.findOne({ email });
 
-    if(userExists) {
-        return res.status(402).json({message: "User already exists"});
-    } else {
-        const newUser = new User({
-            name,
-            email,
-            password
-        })
-        newUser.save();
-        return res.status(200).json(newUser)
-    }
-
-})
-
+  if (userExists) {
+    return res.status(402).json({ message: "User already exists" });
+  } else {
+    const newUser = new User({
+      name,
+      email,
+      password,
+    });
+    newUser.save();
+    return res.status(200).json(newUser);
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`Auth service is running at port ${PORT}`)
-})
+  console.log(`Auth service is running at port ${PORT}`);
+});
--- order-service/index.js
+++ order-service/index.js
@@ -1,57 +1,57 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 9090;
-const mongoose = require("mongoose")
-const jwt = require("jsonwebtoken")
-const amqp = require("amqplib")
-const order = require("./models/order")
-const isAuthenticated = require("./authenticator")
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const amqp = require("amqplib");
+const order = require("./models/order");
+const isAuthenticated = require("./authenticator");
 
 var channel, connection;
 
-mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/order-service", {
+mongoose
+  .connect(process.env.MONGO_URI || "mongodb://localhost/order-service", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
-    console.log("Order SERVICE DB CONNECTED")
-})
+  })
+  .then(() => {
+    console.log("Order SERVICE DB CONNECTED");
+  });
 
 app.use(express.json());
 
 // docker run command: docker run -p 5672:5672 rabbitmq
 async function connect() {
-    const amqpServer = process.env.RABBITMQ_URI || "amqp://localhost:5672";
-    connection = await amqp.connect(amqpServer);
-    channel = await connection.createChannel();
-    await channel.assertQueue("ORDER")
+  const amqpServer = process.env.RABBITMQ_URI || "amqp://localhost:5672";
+  connection = await amqp.connect(amqpServer);
+  channel = await connection.createChannel();
+  await channel.assertQueue("ORDER");
 }
 
-function createOrder(products, userEmail){
- let total = 0;
- for(let t=0; t< products.length; t++ )  {
+function createOrder(products, userEmail) {
+  let total = 0;
+  for (let t = 0; t < products.length; t++) {
     total += products[t].price;
- } 
- const newOrder = new order({
+  }
+  const newOrder = new order({
     products,
     user: userEmail,
     total_price: total,
- })
- newOrder.save();
- return newOrder;
+  });
+  newOrder.save();
+  return newOrder;
 }
 
 connect().then((res) => {
-    channel.consume("ORDER", data => {
-        console.log("Consuming ORDER queue")
-        const {products, userEmail} =  JSON.parse(data.content);
-        const newOrder = createOrder(products, userEmail);
-        channel.ack(data);
-        channel.sendToQueue("PRODUCT", Buffer.from(JSON.stringify({newOrder})
-       ))
-    }); 
+  channel.consume("ORDER", (data) => {
+    console.log("Consuming ORDER queue");
+    const { products, userEmail } = JSON.parse(data.content);
+    const newOrder = createOrder(products, userEmail);
+    channel.ack(data);
+    channel.sendToQueue("PRODUCT", Buffer.from(JSON.stringify({ newOrder })));
+  });
 });
 
-
 app.listen(PORT, () => {
-    console.log(`Order service is running at port ${PORT}`)
-})
+  console.log(`Order service is running at port ${PORT}`);
+});
